228db8ab207849970d19b01c872e77b9a629578c
qalibr <jorgenpettersen@hotmail.com>
2026-02-13 09:47:44 +0100

    initial commit

---
+++ Core.lua
@@ -0,0 +1,54 @@
+-- Author: https://github.com/qalibr
+-- see LICENSE for more details
+
+local Masque_Qalibr, Core = ...
+local Masque, MSQ_Version = LibStub("Masque", true)
+if not Masque then
+    return
+end
+
+local SKIN_NAME = "Qalibr"
+
+local TEXTURE_PATH = "Interface\\AddOns\\" .. Masque_Qalibr .. "\\Assets\\"
+
+local WIDTH = 35.9
+local HEIGHT = 36.5
+
+Masque:AddSkin(SKIN_NAME, {
+    Author = "qalibr",
+    Version = "1.0.0",
+    Masque_Version = MSQ_Version,
+    Description = "A dark, simple skin for Masque.",
+
+    Normal = {
+        Width = WIDTH,
+        Height = HEIGHT,
+        Color = {1, 1, 1, 1},
+        Texture = TEXTURE_PATH .. "Normal"
+    },
+
+    Backdrop = {
+        Width = WIDTH,
+        Height = HEIGHT,
+        Color = {1, 1, 1, 1},
+        Texture = TEXTURE_PATH .. "Backdrop"
+    },
+
+    Pushed = {
+        Width = WIDTH,
+        Height = HEIGHT,
+        Color = {0.8, 0.8, 0.8, 1},
+        Texture = TEXTURE_PATH .. "Normal"
+    },
+
+    Highlight = {
+        Width = WIDTH + 2.1,
+        Height = HEIGHT + 1.5,
+        BlendMode = "ADD",
+        Color = {1, 1, 1, 0.3}
+    },
+
+    Icon = {
+        Mask = TEXTURE_PATH .. "Mask" -- Hack to produce translucent effect
+    }
+})
---
+++ README.md
@@ -0,0 +1,17 @@
+# Qalibr
+
+Custom skin for [Masque](https://github.com/SFX-WoW/Masque).
+
+## Example
+
+<img src="./Img/sans-backdrop.png" alt="drawing" style="width:300px;"/>
+
+*Without backdrop*
+
+<img src="./Img/w-backdrop.png" alt="drawing" style="width:300px;"/>
+
+*With backdrop*
+
+## License
+
+Distributed under modified MIT License. See `LICENSE` for more information.
